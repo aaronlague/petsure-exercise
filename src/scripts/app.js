4dc514f7fb412012aa6a437d58488d1261346936
aaronlague <aaronlague@yahoo.com>
2017-02-08 10:14:35 +1100

    adding fileupload and button

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,16 +4,16 @@ angular.module('app', []).controller('PetController', function ($scope, $http) {
 
 $scope.pets = []; 
 
-//$scope.getTitles = function () { 
-var url = "http://www.mocky.io/v2/589a002e0f00005a0e1f4cb1"
+var url = "http://www.mocky.io/v2/589a457d0f00004f151f4d2d"
 
 $http.get(url).then( function(response) {
   $scope.pets = response.data;
+  $scope.displayPet = function(pet){
+    $scope.petName = pet.petName + '\'s'
+  }
   console.log ($scope.pets)
 });
 
- 
-//}
 
 });
  })();
